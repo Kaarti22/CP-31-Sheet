c923af50b615485ae6b968633a2d93aba044dcde
Kaarti22 <kaartimondi@gmail.com>
2025-06-08 01:18:15 +0530

    friends and the restaurant

---
+++ 1200/D_Friends_and_the_Restaurant.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& a, vi& b, int n){
+        vi v(n);
+        rep(i,0,n){
+            v[i] = b[i] - a[i];
+        }
+        sort(all(v));
+        int ans = 0;
+        int j = n - 1;
+        rep(i,0,j){
+            if(v[i] + v[j] >= 0){
+                ans++;
+                j--;
+            }
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        vi a(n), b(n);
+        inpvec(a, n);
+        inpvec(b, n);
+        Kaarti ob;
+        ob.method(a, b, n);
+    }
+    return 0;
+}
