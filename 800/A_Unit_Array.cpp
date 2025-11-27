23a358d5773a3ef0c7a54cf0811497b77e625443
Kaarti22 <kaartimondi@gmail.com>
2025-11-28 01:39:48 +0530

    unit array

---
+++ 800/A_Unit_Array.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& v, int n){
+        int plus = 0, minus = 0;
+        for(auto it: v){
+            if(it > 0) plus++;
+            else minus++;
+        }
+        int ans = 0;
+        if(minus & 1){
+            ans++;
+            minus--;
+            plus++;
+        }
+        int diff = minus - plus;
+        if(diff > 0){
+            int rem = diff / 2 + (diff & 1);
+            ans += rem;
+            if(rem & 1) ans++;
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        vi v(n);
+        inpvec(v, n);
+        Kaarti ob;
+        ob.method(v, n);
+    }
+    return 0;
+}
