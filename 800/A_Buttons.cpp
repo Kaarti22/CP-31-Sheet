54db0c446f84440fb6a4c68f2d1fc168cdef4d81
Kaarti22 <kaartimondi@gmail.com>
2025-11-28 00:50:40 +0530

    buttons

---
+++ 800/A_Buttons.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int a, int b, int c){
+        if(c & 1){
+            if(b > a){
+                cout<<"Second"<<endl;
+            } else {
+                cout<<"First"<<endl;
+            }
+        } else {
+            if(a > b){
+                cout<<"First"<<endl;
+            } else {
+                cout<<"Second"<<endl;
+            }
+        }
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int a, b, c;
+        cin>>a>>b>>c;
+        Kaarti ob;
+        ob.method(a, b, c);
+    }
+    return 0;
+}
