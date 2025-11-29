a21fbcef386c0216869abbf5ba059e492c36caa4
Kaarti22 <kaartimondi@gmail.com>
2025-11-29 23:52:28 +0530

    make it beautiful

---
+++ 800/A_Make_it_Beautiful.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& v, int n){
+        sort(v.rbegin(), v.rend());
+        vi ans;
+        int sum = 0;
+        while(!v.empty()){
+            int ind = 0;
+            while(ind < v.size() && sum == v[ind]){
+                ind++;
+            }
+            if(ind == v.size()){
+                cout<<"NO"<<endl;
+                return;
+            }
+            ans.pb(v[ind]);
+            sum += v[ind];
+            v.erase(v.begin() + ind);
+            sort(v.rbegin(), v.rend());
+        }
+        cout<<"YES"<<endl;
+        for(auto it: ans){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        vi v(n);
+        inpvec(v, n);
+        Kaarti ob;
+        ob.method(v, n);
+    }
+    return 0;
+}
