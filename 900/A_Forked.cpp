5c20b553aa74dfcb121b881abc47e8024d153d87
Kaarti22 <kaartimondi@gmail.com>
2025-12-01 23:59:34 +0530

    forked

---
+++ 900/A_Forked.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int a, int b, int xk, int yk, int xq, int yq){
+        vector<pair<int, int>> dir = {
+            {a, b}, {a, -b}, {-a, b}, {-a, -b},
+            {b, a}, {b, -a}, {-b, a}, {-b, -a}
+        };
+        set<pair<int, int>> king;
+        set<pair<int, int>> queen;
+        for(auto it: dir){
+            king.insert({xk + it.first, yk + it.second});
+            queen.insert({xq + it.first, yq + it.second});
+        }
+        int ans = 0;
+        for(auto k: king){
+            if(queen.find(k) != queen.end()) ans++;
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int a, b;
+        cin>>a>>b;
+        int xk, yk, xq, yq;
+        cin>>xk>>yk>>xq>>yq;
+        Kaarti ob;
+        ob.method(a, b, xk, yk, xq, yq);
+    }
+    return 0;
+}
