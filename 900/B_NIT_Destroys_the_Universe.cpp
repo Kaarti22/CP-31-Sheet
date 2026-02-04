76ac741f7cbfe61fe49a5fbc3c3cb05b6931bc02
Kaarti22 <kaartimondi@gmail.com>
2026-02-05 02:34:12 +0530

    NIT Destroys the Universe

---
+++ 900/B_NIT_Destroys_the_Universe.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& v, int n){
+        bool zero = 1;
+        rep(i,0,n){
+            if(v[i] != 0){
+                zero = 0;
+                break;
+            }
+        }
+        if(zero){
+            cout<<0<<endl;
+            return;
+        }
+        int st = 0;
+        rep(i,0,n){
+            if(v[i] != 0){
+                st = i;
+                break;
+            }
+        }
+        int end = n - 1;
+        for(int i=n-1; i>=0; i--){
+            if(v[i] != 0){
+                end = i;
+                break;
+            }
+        }
+        zero = 0;
+        rep(i,st,end+1){
+            if(v[i] == 0){
+                zero = 1;
+                break;
+            }
+        }
+        if(zero) cout<<2<<endl;
+        else cout<<1<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        vi v(n);
+        inpvec(v, n);
+        Kaarti ob;
+        ob.method(v, n);
+    }
+    return 0;
+}
