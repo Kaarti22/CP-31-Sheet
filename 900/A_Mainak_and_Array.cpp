57b584f185ed5bacbf2bd0dfeff708a2cd9bce69
Kaarti22 <kaartimondi@gmail.com>
2026-02-05 01:53:58 +0530

    mainak and array

---
+++ 900/A_Mainak_and_Array.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& v, int n){
+        int ans = v[n-1] - v[0];
+        int mini = v[0];
+        rep(i,0,n-1){
+            mini = min(mini, v[i]);
+            ans = max(ans, v[n-1] - mini);
+        }
+        int maxi = v[n-1];
+        for(int i=n-1; i>0; i--){
+            maxi = max(maxi, v[i]);
+            ans = max(ans, maxi - v[0]);
+        }
+        rep(i,1,n){
+            ans = max(ans, v[i-1] - v[i]);
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        vi v(n);
+        inpvec(v, n);
+        Kaarti ob;
+        ob.method(v, n);
+    }
+    return 0;
+}
