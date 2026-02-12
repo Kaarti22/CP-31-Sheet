c33620a11562b6d4ae0d8a7de0b74d40c92b04fd
Kaarti22 <kaartimondi@gmail.com>
2026-02-12 23:08:10 +0530

    exciting bets

---
+++ 900/A_Exciting_Bets.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int a, int b){
+        if(a > b) swap(a, b);
+        int diff = b - a;
+        if(diff == 0){
+            cout<<"0 0"<<endl;
+            return;
+        }
+        int left = diff * (a / diff);
+        int right = (left == a) ? left : left + diff;
+        int moves = min(a - left, right - a);
+        cout<<diff<<" "<<moves<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int a, b;
+        cin>>a>>b;
+        Kaarti ob;
+        ob.method(a, b);
+    }
+    return 0;
+}
